ac588ca01a48ade730796c9ac1d33507237cf262
Abderrahmen Brinis <72577453+dahmenbrinis@users.noreply.github.com>
2021-11-19 19:07:20 +0100

    added the exportAllToExcel function into requisition.index component, and added the button in navigation menu to fire the event

--- app/View/Requisition/Index.php
+++ app/View/Requisition/Index.php
@@ -2,12 +2,24 @@
 
 namespace App\View\Requisition;
 
+use App\Exports\RequisitionsExport;
 use App\Models\Person;
 use Livewire\Component;
 use Livewire\WithPagination;
+use Maatwebsite\Excel\Facades\Excel;
 
 class Index extends Component
 {
+    protected $listeners = [
+        'exportAllToExcel'=>'exportAllToExcel',
+//        'importAllFromExcel'=>'exportAllToExcel',
+//        'exportRequisition'=>'exportAllToExcel',
+    ];
+
+    public function exportAllToExcel()
+    {
+        return Excel::download(new RequisitionsExport,'users.xlsx');
+    }
     public function render()
     {
         return view('requisition.index');
