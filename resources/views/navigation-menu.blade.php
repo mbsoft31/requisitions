2bad5378402e995c6cf3b92f1e5de2cd13cf1759
Abderrahmen Brinis <72577453+dahmenbrinis@users.noreply.github.com>
2021-11-20 21:54:13 +0100

    added the logo to the navigation and login

--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -6,15 +6,16 @@
                 <!-- Logo -->
                 <div class="flex items-center flex-shrink-0">
                     <a href="{{ route('dashboard') }}">
-                        <x-jet-application-mark class="block w-auto h-9" />
+{{--                        <x-jet-application-mark class="block w-auto h-9" />--}}
+                        <img src="{{asset('images/logo.png')}}" alt="logo" class="object-fill h-12 ">
                     </a>
                 </div>
 
                 <!-- Navigation Links -->
                 <div class="hidden space-x-8 sm:-my-px sm:flex">
-                    <x-jet-nav-link href="{{ route('dashboard') }}" :active="request()->routeIs('dashboard')">
-                        {{ __('Dashboard') }}
-                    </x-jet-nav-link>
+{{--                    <x-jet-nav-link href="{{ route('dashboard') }}" :active="request()->routeIs('dashboard')">--}}
+{{--                        {{ __('Dashboard') }}--}}
+{{--                    </x-jet-nav-link>--}}
                         <x-jet-nav-link href="{{ route('requisition.index') }}" :active="request()->routeIs('requisition.index')">
                             {{ __('Requisitions') }}
                         </x-jet-nav-link>
@@ -95,9 +96,9 @@
     <!-- Responsive Navigation Menu -->
     <div :class="{'block': open, 'hidden': ! open}" class="hidden sm:hidden">
         <div class="pt-2 pb-3 space-y-1">
-            <x-jet-responsive-nav-link href="{{ route('dashboard') }}" :active="request()->routeIs('dashboard')">
-                {{ __('Dashboard') }}
-            </x-jet-responsive-nav-link>
+{{--            <x-jet-responsive-nav-link href="{{ route('dashboard') }}" :active="request()->routeIs('dashboard')">--}}
+{{--                {{ __('Dashboard') }}--}}
+{{--            </x-jet-responsive-nav-link>--}}
             <x-jet-responsive-nav-link href="{{ route('requisition.index') }}" :active="request()->routeIs('requisition.index')">
                 {{ __('Requisitions') }}
             </x-jet-responsive-nav-link>
